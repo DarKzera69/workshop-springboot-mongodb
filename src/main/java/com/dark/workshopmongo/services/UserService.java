36d2e8af1764fc4fb4ec7e0529e769e8220188c5
Jorge Luiz <jorgeluiz006@hotmail.com>
2024-02-22 12:49:43 -0300

    insercao de usuario com POST

--- src/main/java/com/dark/workshopmongo/services/UserService.java
+++ src/main/java/com/dark/workshopmongo/services/UserService.java
@@ -1,26 +1,37 @@
 package com.dark.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dark.workshopmongo.domain.User;
+import com.dark.workshopmongo.dto.UserDTO;
 import com.dark.workshopmongo.repository.UserRepository;
+import com.dark.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
-		User user = repo.findById(id).orElse(null);
-		return user;
-		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
